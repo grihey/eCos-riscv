8e9ba00318e5c6fa21e72657cf7c19c8d02f8886
Grigoriy Romanov <grigoriy.romanov@syntacore.com>
2017-02-27 18:03:40 +0300

    gnutools untracked, ecos.db have template board

--- gnutools/arm-eabi/lib/gcc/arm-eabi/4.6.3/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../../../../src/gcc-4.6.3/configure --target=arm-eabi --with-cpu=arm7tdmi --prefix=/opt/ecos/gnutools/arm-eabi --with-newlib --with-gnu-as --with-gnu-ld --with-gmp=/opt/gmp-4.3.2 --with-mpfr=/opt/mpfr-2.4.2 --with-mpc=/opt/mpc-0.8.2 --enable-languages=c,c++ --enable-threads --disable-hosted-libstdcxx --disable-__cxa_atexit --disable-libquadmath --disable-decimal-float -with-pkgversion='eCos GNU Tools 4.6.3-20120623' --with-bugurl=http://bugs.ecos.sourceware.org/";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "arm7tdmi" } };
